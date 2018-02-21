86109b8f38a5c03475ebb46ae1ee7a2d6319d9f3
Ovidiu - Dan Bogat <ovidiu@pro-odb.com>
2018-02-21 16:20:53 +0100

    Added hello.cs and Makefile

---
+++ tasks/Task1/hello.cs
@@ -0,0 +1,30 @@
+/* 
+ * =====================================================================================
+ *
+ *       Filename:  hello.cs
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/21/2018 04:17:17 PM
+ *       Revision:  none
+ *       Compiler:  mcs -- Mono C# compiler version 4.4.1.0
+ *
+ *         Author:  Ovidiu - Dan Bogat [ic17b501], ic17b501@technikum-wien.at
+ *   Organization:  -
+ *
+ * =====================================================================================
+ */
+using System;
+
+namespace Task1
+{
+	public class hello
+	{
+		public static void Main(string[] args)
+			{	
+				Console.Write("Hello World!");
+				Console.ReadKey();
+			}
+	}
+}
