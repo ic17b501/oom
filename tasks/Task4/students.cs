a736a34146db702128ca52e85adf8f0631eb20a2
Ovidiu - Dan Bogat <ic17b501@technikum-wien.at>
2018-04-02 08:58:45 -0400

    Added correct files, interfaces, expanded Assistant to inherit from student, added tests-cases, added test case for exception

--- tasks/Task4/students.cs
+++ tasks/Task4/students.cs
@@ -17,9 +17,90 @@
  */
 using System;
 
+using Items;
+using Persons;
+
+namespace Items
+{
+    public interface IItem
+    {
+	/* Some shall have setters, other shallen't */
+	/* Defined at random by me */
+	/* Deal with it */
+	string Description {get;}
+	decimal Value {get;}
+	int Count {get;}
+	bool Legendary {get;}
+	bool Unique {get;}
+	bool Consumable {get;set;}
+    }
+
+    public class Boots : IItem
+    {
+	private decimal value;
+	private int count;
+	private bool legendary;
+	private bool unique;
+		
+	public Boots()
+	    {
+		Random rnd = new Random(Guid.NewGuid().GetHashCode());
+		this.value = rnd.Next(0,100);
+		this.count = 2;
+		this.legendary = rnd.Next(100) <= 20 ? true : false;
+		this.unique =	rnd.Next(100) <= 20 ? true : false;
+		this.Consumable =	rnd.Next(100) <= 20 ? true : false;
+	    }
+		
+	public string Description
+	{
+	    get { return "Boots to boot your feet.";}
+	}
+	public decimal Value
+	{
+	    get { return this.value; }
+	}
+
+	public int Count
+	{
+	    get { return this.count; }
+	}
+
+	public bool Legendary
+	{
+	    get { return this.legendary; } 
+	}
+
+	public bool Unique
+	{
+	    get { return this.unique; }
+	}
+
+	public bool Consumable
+	{
+	    get { return this.Consumable; }
+	    set { this.Consumable = value; }
+	}
+    }
+	
+}
+
+namespace Persons
+{
+    public interface IPerson
+    {
+	string FirstName {get;set;}
+	string LastName {get;set;}
+	string FullName {get;}
+	double DoNothing();
+    }
+
+}
+
+
 namespace Students
 {
-    public class Student
+    public class Student:IPerson
     {
 	private string firstName;
 	private string lastName;
@@ -32,10 +113,10 @@ namespace Students
 	private bool humor;
 	
 	public Student()
-	{
-	    this.initEmpty();
-	    Console.WriteLine("Empty student created");
-	}
+	    {
+		this.initEmpty();
+		Console.WriteLine("Empty student created");
+	    }
 
 	public Student(string firstName,
 		       string lastName,
@@ -45,15 +126,15 @@ namespace Students
 		       bool humor=false,
 		       string mail="<none>"
 		       )
-	{
-	    this.firstName = firstName;
-	    this.lastName = lastName;
-	    this.matNumber = matNumber;
-	    this.telNumber = telNumber;
-	    this.gold = gold;
-	    this.email = mail;
-	    this.humor = humor;
-	}
+	    {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.matNumber = matNumber;
+		this.telNumber = telNumber;
+		this.gold = gold;
+		this.email = mail;
+		this.humor = humor;
+	    }
 
 	private void initEmpty()
 	{
@@ -74,7 +155,7 @@ namespace Students
 	    set {this.firstName = value;}
 	}
 
-	public string LasName
+	public string LastName
 	{
 	    get {return this.lastName;}
 	    set {this.lastName = value;}
@@ -126,6 +207,11 @@ namespace Students
 	    get {return this.humor;}
 	    set {this.humor = value;}
 	}
+
+	public double DoNothing()
+	{
+	    throw new NotSupportedException();
+	}
 	
 	public void print()
 	{
@@ -144,4 +230,27 @@ namespace Students
 
 
     }
+
+    public class Assistant:Student
+    {
+	public Assistant() : base() {}
+	
+	public Assistant(string firstName,
+		       string lastName,
+		       string matNumber,
+		       string telNumber,
+		       double gold,
+		       bool humor=false,
+		       string mail="<none>"
+			 ) : base(
+				  firstName,
+				  lastName,
+				  matNumber,
+				  telNumber,
+				  gold,
+				  humor,
+				  mail
+				  ) {}
+    }
+
 }
--- tasks/Task4/tests/students_ut.cs
+++ tasks/Task4/tests/students_ut.cs
@@ -42,5 +42,53 @@ namespace NUnit.Tests
 
 	    }
 
+	    [Test]
+		public void Assistants1()
+	    {
+		Assistant a = new Assistant();
+		Assert.That(a, Is.Not.Null);
+	    }
+
+	    [Test]
+		public void Assitants2()
+	    {
+		Assistant a = new Assistant(
+					"Helpful",
+					"Marty",
+					"a1234567890",
+					"x0987654321",
+					199,
+					false,
+					"mail@box.com"
+					);
+		Assert.That(a, Is.Not.Null);
+		Assert.That(a.MatriculationNumber, Does.StartWith("a"));
+		Assert.That(a.TelNumber, Does.Not.StartWith("a"));
+		Assert.That(a.FullName, Does.Contain("Mar"));
+		Assert.That(a.Email, Does.Not.Contain("non"));
+		
+	    }
+
+
+
+	    [Test]
+		public void Assitants3()
+	    {
+		Assistant a = new Assistant(
+					"Helpful",
+					"Marty",
+					"a1234567890",
+					"x0987654321",
+					199,
+					false,
+					"mail@box.com"
+					);
+		Assert.That(a, Is.Not.Null);
+		Assert.That(a.MatriculationNumber, Does.StartWith("a"));
+		Assert.That(a.TelNumber, Does.Not.StartWith("a"));
+		Assert.That(a.FullName, Does.Contain("Mar"));
+		Assert.That(a.Email, Does.Not.Contain("non"));
+		Assert.That(a.DoNothing, Throws.InstanceOf<NotSupportedException>());
+	    }
 	}
 }
