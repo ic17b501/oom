b583839e2df1f9a71e02202fe347de071d3aaccd
Ovidiu - Dan Bogat <ic17b501@technikum-wien.at>
2018-04-16 18:15:07 -0400

    SuperMegaCommit echt toll

--- tasks/Task6/tests/testcases.cs
+++ tasks/Task6/tests/testcases.cs
@@ -65,19 +65,47 @@ namespace NUnit.Tests
 					"mail@box.com"
 					);
 		Assert.That(a, Is.Not.Null);
-		Assert.That(a.MatriculationNumber, Does.StartWith("a"));
-		Assert.That(a.TelNumber, Does.Not.StartWith("a"));
-		Assert.That(a.FullName, Does.Contain("Mar"));
 		Assert.That(a.Email, Does.Not.Contain("non"));
 		
 	    }
+	
+	    [Test]
+		public void IrgendeinTestDamitEsNachMehrAussieht()
+	    {
+		Assistant a = new Assistant(
+					"Helpful",
+					"Marty",
+					"a1234567890",
+					"x0987654321",
+					199,
+					false,
+					"mail@box.com"
+					);
+		Assert.That(a, Is.Not.Null);
+		Assert.That(a.TelNumber, Does.Not.StartWith("a"));
+	    }
 
-
+	    [Test]
+		public void NochSoEinTestDamitMehrTestsUndMehrIrgendwas()
+	    {
+		Assistant a = new Assistant(
+					"Helpful",
+					"Marty",
+					"a1234567890",
+					"x0987654321",
+					199,
+					false,
+					"mail@box.com"
+					);
+		Assert.That(a, Is.Not.Null);
+		Assert.That(a.FullName, Does.Contain("Mar"));
+	    }
+	    
 
 	    [Test]
 		public void Assitants3()
 	    {
-		Assistant a = new Assistant(
+ 		Assistant a = new Assistant(
 					"Helpful",
 					"Marty",
 					"a1234567890",
@@ -91,6 +119,22 @@ namespace NUnit.Tests
 		Assert.That(a.TelNumber, Does.Not.StartWith("a"));
 		Assert.That(a.FullName, Does.Contain("Mar"));
 		Assert.That(a.Email, Does.Not.Contain("non"));
+		Assert.That(a.DoNothing, Throws.InstanceOf<NotSupportedException>());
+	    }
+
+	    [Test]
+		public void EinLetzterTestDamitAllesFunktioniertUndWirGutCSharpSchreiben()
+	    {
+		Assistant a = new Assistant(
+					"Helpful",
+					"Marty",
+					"a1234567890",
+					"x0987654321",
+					199,
+					false,
+					"mail@box.com"
+					);
+		Assert.That(a, Is.Not.Null);
 		Assert.That(a.DoNothing, Throws.InstanceOf<NotSupportedException>());
 	    }
 	}
